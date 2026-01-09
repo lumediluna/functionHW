8c44f1ead7cc42b66ff75ab1029358c046b9fdaf
Olga <lumediluna007@gmail.com>
2026-01-09 16:51:30 +0300

    hw

---
+++ tests/app.test.js
@@ -0,0 +1,63 @@
+import {nameIsValid} from '../src/app.js';
+import {fullTrim} from '../src/app.js';
+import{getTotal} from '../src/app.js';
+
+
+describe ('Проверка имени пользователя', () => {
+    describe ('nameIsValid', () => {
+    test ('Имя должно состоять из букв' , async () => {
+      const name = 'bob';
+      const result = await nameIsValid(name);
+    expect (result).toBe(true);
+     });
+   });
+});
+    test('Имя короче 2 букв — невалидно',  async () => {
+        const name = 's';
+        const result = await nameIsValid(name);
+    expect (result).toBe(false);
+    });
+    test('Имя - цифрами - невалидно', async () => {
+      const name = '77';
+      const result = await nameIsValid(name);
+    expect(result).toBe(false);
+    });
+   
+  
+
+describe('Удаление пробелов из строки', () => {
+  describe('fullTrim', () => {
+  test ('Удаление пробелов из строки', async()=> {
+    const text = ' f    g   jjj';
+    const expected = 'fgjjj';
+    const result = await fullTrim(text);
+  expect(result).toBe(expected)
+    });
+  });
+});
+
+  test ('Удаление переноса', async () => {
+    const text = '\n';
+    const expected = '';
+    const result = await fullTrim(text);
+  expect(result).toBe(expected);
+  });
+
+  test('Пустая строка при text=null', async () => {
+    const text = null;
+    const expected= '';
+    const result = await fullTrim(text);
+  expect(result).toBe(expected);
+
+  });
+
+describe('возможные ошибки в скидке getTotal', () => {
+  test.each([
+    ['скидка не число', [{ price: 10, quantity: 1 }], 'оо', 'Скидка должна быть числом'],
+    ['скидка не может быть отрицательной', [{ price: 10, quantity: 1 }], -1, 'Процент скидки должен быть от 0 до 99'],
+    ['скидка больше 99%', [{ price: 10, quantity: 1 }], 100, 'Процент скидки должен быть от 0 до 99'],
+  ])('getTotal %', (_title, items, discount, errorMessage) => {
+    const result = () => getTotal(items, discount);
+  expect(result).toThrow(errorMessage);
+  });
+});
