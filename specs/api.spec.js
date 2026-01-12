026eaaf2e9314aa0c92de26fbc72d2e628648b18
olgklsn <lumediluna007@gmail.com>
2026-01-12 23:04:04 +0300

    api tests

--- specs/api.spec.js
+++ specs/api.spec.js
@@ -1,6 +1,6 @@
 describe('Создание пользователя', () => {
-  test('Неуспешное создание пользователя, логин уже используется', async () => {
-    const res = await fetch('https://bookstore.demoqa.com/Account/v1/User', {
+  test("Неуспешное создание пользователя, логин уже используется", async () => {
+    const result = await fetch('https://bookstore.demoqa.com/Account/v1/User', {
       method: 'POST',
       headers: {
         accept: 'application/json',
@@ -12,8 +12,88 @@ describe('Создание пользователя', () => {
       }),
     });
 
-    expect(res.status).toBe(400); 
-    const body = await res.json();
+    expect(result.status).toBe(400); 
+    const body = await result.json();
+    expect(body.message).toBeDefined();
+  });
+
+  test("Успешное создание пользователя", async () => {
+  const result = await fetch('https://bookstore.demoqa.com/Account/v1/User', {
+    method: 'POST',
+    headers: {
+      accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      userName: 'Olga1_64646464644' , 
+      password: 'P@ssw0rd123!',       
+    }),
+  });
+
+  const body = await result.json();
+  expect(result.status).toBe(201);
+  expect(body.userID).toBeDefined();
+});
+
+
+
+  test("Неуспешное создание пользователя, пароль не подходит", async () => {
+    const result = await fetch('https://bookstore.demoqa.com/Account/v1/User', {
+      method: 'POST',
+      headers: {
+        accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        userName: 'Olga_6464664644',
+        password: '456',
+      }),
+    });
+
+    expect(result.status).toBe(400); 
+    const body = await result.json();
     expect(body.message).toBeDefined();
   });
 });
+
+test ("Успешная генерация токена", async () => {
+const result = await fetch('https://bookstore.demoqa.com/Account/v1/GenerateToken', {
+      method: 'POST',
+      headers: {
+        accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+      userName: 'Olga_6464664644' , 
+      password: 'P@ssw0rd123!',   
+      }),
+    });
+
+    expect(result.status).toBe(200);
+  const body = await result.json();
+  expect(body.token).toBeDefined();
+  });
+
+  test ("Неуспешная генерация токена", async () => {
+const result = await fetch('https://bookstore.demoqa.com/Account/v1/GenerateToken', {
+      method: 'POST',
+      headers: {
+        accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+      userName: 'string', 
+      password: 'string',   
+      }),
+    });
+  const body = await result.json();  
+  expect(result.status).toBe(200);
+  expect(body.status).toBe('Failed'); 
+  expect(body.result).toBe('User authorization failed.');
+  });
+
+
+
+
+
+
