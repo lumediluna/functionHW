d47359ff932ae2e282e395a82f23c7b6596847e2
olgklsn <lumediluna007@gmail.com>
2026-01-12 22:14:25 +0300

    создание пользователя 400

--- specs/api.spec.js
+++ specs/api.spec.js
@@ -1,20 +1,19 @@
-describe ("Создание пользователя"), () => {
-test(' Успешное создание пользователя', async () => {
-  const res = await fetch('https://bookstore.demoqa.com/Account/v1/User', {
-    method: 'POST',
-    headers: {
-      'accept': 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      userName: 'string',
-      password: 'string',
-    }),
-  });
+describe('Создание пользователя', () => {
+  test('Неуспешное создание пользователя, логин уже используется', async () => {
+    const res = await fetch('https://bookstore.demoqa.com/Account/v1/User', {
+      method: 'POST',
+      headers: {
+        accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        userName: 'string',
+        password: 'string',
+      }),
+    });
 
-  expect(res.status).toBe(201); // или тот код, который ждёшь
- const body = await res.json();
-  // дальше проверки по ответу
-  expect(body.userID).toBeDefined();
+    expect(res.status).toBe(400); 
+    const body = await res.json();
+    expect(body.message).toBeDefined();
+  });
 });
-};
