72d1e0da65736cdb8111e6c213bfeab1360ca05a
olgklsn <lumediluna007@gmail.com>
2026-01-19 22:56:38 +0300

    настроила линтеры

---
+++ sand.js
@@ -0,0 +1,68 @@
+// let user ={ // литеральная ноатция
+//     name: "Olga",
+//     age: 37,
+//     "two words": true,
+// }; 
+// console.log(user);
+    
+//  user.isAdmin =false; // true/false
+
+// console.log(user)
+
+
+// let fruits =[
+//     "apple", 
+//     "pear", 
+//     "orange", 
+//     {name:"Olga"}, 
+//  //   function() {return "Hello"},
+// ]
+//fruits[5]= "banana"
+
+// console.log(fruits);
+// console.log(fruits.at(-1));
+// delete fruits.at[1];
+// fruits.splice(3,1);
+// console.log(fruits);
+//fruits.pop()
+// console.log(fruits.pop())
+// console.log(fruits)
+// fruits.push("cherry")
+//console.log(fruits);
+//fruits.unshift("watermelon")
+//fruits.shift("Cherry")
+
+// console.log(fruits.shift());
+// console.log(fruits.unshift());
+// for (let i=0; i< fruits.length; i++) {
+// console.log(fruits[i])
+// }
+
+// for (let fruit of fruits) {
+//     console.log(fruit);
+// }
+// fruits[1000] = "broccoli"; 
+// fruits.length = 1500;
+// fruits.length= 1;
+// console.log(fruits.length);
+
+//let numbers =[1, 2, 3, 4, 5]
+
+//let doubled = numbers.map(num => num *2)
+// numbers.forEach(num => {
+//     console.log(num);
+// })
+
+// console.log(doubled)
+//console.log(numbers.every(num => num % 2 === 0));
+//console.log(numbers.some(num => num % 2 === 0));
+//console.log(numbers.find(num => num % 12 === 0));
+// //console.log(numbers.findIndex(num => num % 12 === 0));
+
+// let users = [
+//     {name: "Ann", age: 22},
+//     {name: "bob", age:3},
+// ]
+// console.log(users[0], age)
+
+
