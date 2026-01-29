e4e078ded5cc87efc7d8920f7ff30fd630448e3f
olgklsn <lumediluna007@gmail.com>
2026-01-29 20:28:21 +0300

    поправила package

--- framework/config/configBookstore.js
+++ framework/config/configBookstore.js
@@ -5,7 +5,7 @@ export const config = {
   baseURL: 'https://demoqa.com',
   defaultPassword: 'P@ssw0rd123!',
 };
-=======
+
 // Object.freeze используем, чтобы запретить изменять конфиг,
 // конфиг только для чтения
 export const baseURL = 'https://bookstore.demoqa.com'
