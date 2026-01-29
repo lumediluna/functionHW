71daa36bdbed44c472540f0539be7ce57f7caef8
olgklsn <lumediluna007@gmail.com>
2026-01-29 20:46:25 +0300

    apiwithcont

--- framework/config/configBookstore.js
+++ framework/config/configBookstore.js
@@ -1,6 +1,5 @@
 import 'dotenv/config'
 
-array
 export const config = {
   baseURL: 'https://demoqa.com',
   defaultPassword: 'P@ssw0rd123!',
@@ -9,4 +8,4 @@ export const config = {
 // Object.freeze используем, чтобы запретить изменять конфиг,
 // конфиг только для чтения
 export const baseURL = 'https://bookstore.demoqa.com'
-units
+
--- tests/apiWithCont.test.js
+++ tests/apiWithCont.test.js
@@ -13,52 +13,36 @@ test('Создание книги', async ({ request }) => {
   const userName = faker.internet.username();
   const password = config.defaultPassword;
 
-  // 1. Создаём пользователя
+  
   const createUser = await account.createUser(userName, password);
   expect(createUser.status()).toBe(201);
-
   const { userID } = await createUser.json();
-
-  // 2. Логинимся
   const login = await account.generateToken(userName, password);
   expect(login.status()).toBe(200);
-
   const { token } = await login.json();
-
-  // 3. Добавляем книгу
   const addBook = await bookstore.addBook(
     token,
     userID,
     '9781449325862'
   );
-
   expect(addBook.status()).toBe(201);
 });
+
 
 test('Добавление и обновление книги', async ({ request }) => {
-
   const account = new AccountController(request, config.baseURL);
   const bookstore = new BookStoreController(request, config.baseURL);
-
   const userName = faker.internet.username();
   const password = config.defaultPassword;
-
-  // 1. Пользователь
   const createUser = await account.createUser(userName, password);
   expect(createUser.status()).toBe(201);
   const { userID } = await createUser.json();
-
-  // 2. Авторизация
   const login = await account.generateToken(userName, password);
   expect(login.status()).toBe(200);
   const { token } = await login.json();
-
-  // 3. Добавление книги
   const oldIsbn = '9781449325862';
   const addBook = await bookstore.addBook(token, userID, oldIsbn);
   expect(addBook.status()).toBe(201);
-
-  // 4. Обновление книги
   const newIsbn = '9781449331818';
   const updateBook = await bookstore.updateBook(
     token,
@@ -66,33 +50,24 @@ test('Добавление и обновление книги', async ({ request
     oldIsbn,
     newIsbn
   );
-
-  expect(updateBook.status()).toBe(200);
+ expect(updateBook.status()).toBe(200);
 });
-test('Добавление, обновление и получение книги', async ({ request }) => {
 
+
+test('Добавление, обновление и получение книги', async ({ request }) => {
   const account = new AccountController(request, config.baseURL);
   const bookstore = new BookStoreController(request, config.baseURL);
-
   const userName = faker.internet.username();
   const password = config.defaultPassword;
-
-  // 1. Пользователь
   const createUser = await account.createUser(userName, password);
   expect(createUser.status()).toBe(201);
   const { userID } = await createUser.json();
-
-  // 2. Авторизация
   const login = await account.generateToken(userName, password);
   expect(login.status()).toBe(200);
   const { token } = await login.json();
-
-  // 3. Добавление книги
   const oldIsbn = '9781449325862';
   const addBook = await bookstore.addBook(token, userID, oldIsbn);
   expect(addBook.status()).toBe(201);
-
-  // 4. Обновление книги
   const newIsbn = '9781449331818';
   const updateBook = await bookstore.updateBook(
     token,
@@ -101,47 +76,33 @@ test('Добавление, обновление и получение книг�
     newIsbn
   );
   expect(updateBook.status()).toBe(200);
-
-  // 5. Получение книги
   const getBook = await bookstore.getBook(newIsbn);
   expect(getBook.status()).toBe(200);
-
   const bookInfo = await getBook.json();
-
-  // Минимальные проверки
   expect(bookInfo.isbn).toBe(newIsbn);
   expect(bookInfo.title).toBeDefined();
   expect(bookInfo.author).toBeDefined();
 });
-test('Удаление книги', async ({ request }) => {
 
+
+test('Удаление книги', async ({ request }) => {
   const account = new AccountController(request, config.baseURL);
   const bookstore = new BookStoreController(request, config.baseURL);
-
   const userName = faker.internet.username();
   const password = config.defaultPassword;
-
-  // 1. Пользователь
   const createUser = await account.createUser(userName, password);
   expect(createUser.status()).toBe(201);
   const { userID } = await createUser.json();
-
-  // 2. Авторизация
   const login = await account.generateToken(userName, password);
   expect(login.status()).toBe(200);
   const { token } = await login.json();
-
-  // 3. Добавление книги
   const isbn = '9781449325862';
   const addBook = await bookstore.addBook(token, userID, isbn);
   expect(addBook.status()).toBe(201);
-
-  // 4. Удаление книги
   const deleteBook = await bookstore.deleteBook(
     token,
     userID,
     isbn
   );
-
   expect(deleteBook.status()).toBe(204);
 });
